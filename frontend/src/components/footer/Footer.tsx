3907fde673c8914ba2229823757c8690ee932ac4
Anushree More <moreanushree2@gmail.com>
2026-02-03 20:29:41 +0530

    setup basic UI

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-title">E-Com Dashboard</p>
+      <span className="footer-sub">Built with ❤️ using React</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/nav-bar/NavBar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+      <ul className="nav-ul">
+        <li>
+          <Link to={"/"}>Products</Link>
+        </li>
+        <li>
+          <Link to={"/add"}>Add Products</Link>
+        </li>
+        <li>
+          <Link to={"/update"}>Update Products</Link>
+        </li>
+        <li>
+          <Link to={"/logout"}>Logout</Link>
+        </li>
+        <li>
+          <Link to={"/profile"}>Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
